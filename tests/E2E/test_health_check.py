ed96bda8c2393d2310f94fd213095d8d5d217a43
Spanarchian <spanarchian@gmail.com>
2023-04-17 18:31:26 +0100

    ADDED: missing httpx

--- tests/E2E/test_health_check.py
+++ tests/E2E/test_health_check.py
@@ -4,7 +4,7 @@ from api.api import api
 
 client = TestClient(api)
 
-def test_read_main():
+def test_root():
     response = client.get("/")
     assert response.status_code == 200
     assert response.json() == {"message": "I am alive!"}
